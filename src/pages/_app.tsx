b60989967a0e492c50c55bafd4b8e4dfb53f136f
Kryštof Matějka <kx.matejka@gmail.com>
2021-02-28 19:04:35 +0100

    setup eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,8 +11,8 @@ const GlobalStyle = createGlobalStyle`
 
 const SpringApp = ({Component, pageProps}) => {
   return (
-    <>
-      <GlobalStyle />
+    <> 
+      <GlobalStyle/>
       <Component {...pageProps} />
     </>
   )
