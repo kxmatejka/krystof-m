9d1d3bbada719de16acadb8455eb71a87a4646b5
Kryštof Matějka <kx.matejka@gmail.com>
2021-03-11 21:46:03 +0100

    add animations to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,49 @@
 import React, {FC} from 'react'
 import styled from 'styled-components'
-import {LinkBlog, SvgBase64Img, TYPESCRIPT_ICON, REACT_ICON, NODEJS_ICON, AWS_ICON} from '../components'
+import {motion} from 'framer-motion'
+import {SvgBase64Img, TYPESCRIPT_ICON, REACT_ICON, NODEJS_ICON, AWS_ICON} from '../components'
 
-const Container = styled.div`
+const Container = styled(motion.div)`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100%;
 `
 
-const Background = styled.div`
-  width: 420px;
-  height: 420px;
-  background: var(--dark);
-  border-radius: 50%;
-  box-shadow: 0 0 100px 0 rgba(0,0,0,0.5);
-
+const HeaderContainer = styled.div`
+  flex-grow: 1;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
-
-  @media only screen and (max-width: 859px)  {
-    border-radius: 0;
-    width: 100%;
-    height: 100%;
-  }
+  height: 100%;
 `
 
-const Heading = styled.h1`
-  font-size: 2.6rem;
+interface GapProps {
+  width?: string
+  height?: string
+}
+
+const Gap = styled.div<GapProps>`
+  width: ${p => p.width ? p.width : '100%'};
+  height: ${p => p.height ? p.height : '0'};
+`
+
+const Heading = styled(motion.h1)`
+  font-size: 3rem;
   margin: 0;
   text-align: center;
+  color: var(--dark);
 `
 
-const Description = styled.p`
+const IconsContainer = styled.div`
   display: flex;
-  justify-content: center;
-  margin: 20px 0 10px 0;
+  flex-direction: row;
+  
+  & > :not(:last-child) {
+    margin-right: 20px;
+  }
 `
 
 const TypescriptIcon = () => {
@@ -64,23 +70,144 @@ const AwsIcon = () => {
   )
 }
 
-const Links = styled.p`
-  text-align: center;
-  margin: 0;
+const StyledCircle = styled(motion.div)`
+  width: 75px;
+  height: 75px;
+  border-radius: 50%;
+  background: var(--dark);
+  color: var(--light);
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: initial;
+  align-items: center;
 `
+
+const Circle: FC = ({children}) => {
+  return (
+    <StyledCircle
+      initial={{
+        opacity: 0,
+      }}
+      variants={{
+        open: {
+          opacity: 1,
+        },
+      }}
+    >
+      {children}
+    </StyledCircle>
+  )
+}
+
+const CirclesVariants = {
+  open: {
+    transition: {
+      staggerChildren: 0.35,
+    },
+  },
+}
+
+const CvContainer = styled(motion.div)`
+  height: 200px;
+`
+
+const StyledLinkCv = styled(motion.div)`
+  opacity: 1;
+  color: var(--dark);
+  font-weight: bold;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Arrow = styled.div`
+  width: 10px;
+  height: 10px;
+  border-top: 3px solid var(--dark);
+  border-right: 3px solid var(--dark);
+  border-bottom: 0;
+  border-left: 0;
+
+  transform: rotate(135deg);
+`
+
+const BottomArrow = styled(Arrow)`
+  margin-top: -5px;
+`
+
+const LinkCv = () => {
+  return (
+    <CvContainer
+      initial={{
+        opacity: 0,
+      }}
+      variants={{
+        open: {
+          opacity: 1,
+        },
+      }}
+    >
+      <StyledLinkCv
+        variants={{
+          open: {
+            opacity: [1, 1],
+            y: [0, 25, 0],
+          },
+        }}
+        transition={{
+          duration: 2,
+          loop: Infinity,
+        }}
+      >
+        <label>Full CV</label>
+        <Arrow/>
+        <BottomArrow/>
+      </StyledLinkCv>
+    </CvContainer>
+  )
+}
 
 const Home: FC = () => {
   return (
-    <Container>
-      <Background>
-        <Heading>Kryštof Matějka</Heading>
-        <Description>
-          I like&nbsp;<TypescriptIcon/>, love&nbsp;<ReactIcon/>, do&nbsp;<NodeJsIcon/>&nbsp;and also&nbsp;<AwsIcon/>
-        </Description>
-        <Links>
-          CV | <LinkBlog>Blog</LinkBlog>
-        </Links>
-      </Background>
+    <Container
+      animate='open'
+      variants={CirclesVariants}
+    >
+      <HeaderContainer>
+        <Heading
+          initial={{
+            opacity: 0,
+          }}
+          variants={{
+            open: {
+              opacity: 1,
+            },
+          }}
+        >Kryštof Matějka</Heading>
+        <Gap height='20px'/>
+        <IconsContainer>
+          <Circle>
+            <label>I love</label>
+            <ReactIcon/>
+          </Circle>
+          <Circle>
+            <label>I like</label>
+            <TypescriptIcon/>
+          </Circle>
+          <Circle>
+            <label>I do</label>
+            <NodeJsIcon/>
+          </Circle>
+          <Circle>
+            <label>&</label>
+            <AwsIcon/>
+          </Circle>
+        </IconsContainer>
+      </HeaderContainer>
+      <LinkCv/>
     </Container>
   )
 }
